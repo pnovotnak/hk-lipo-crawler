d4b6bccf5845c0a355f755eca789de21d31de5d2
Peter Novotnak <pnovotnak@flexion.us>
2014-09-24 15:04:32 -0700

    pep8 cleanup

--- crawler.py
+++ crawler.py
@@ -5,45 +5,60 @@ import requests
 import re
 
 
-class Crawler:
-    '''
+class Crawler(object):
+    """
     A crawler object. Stores product info.
-    '''
+    """
 
-    re_usd = re.compile("\$[0-9]+\.[0-9]+")
-    re_desc = re.compile('<.*?>')
-    re_mah = re.compile('(?<=\s)[0-9]+(?=mah\s)', re.IGNORECASE)
-    re_cells = re.compile('(?<=\s)[0-9]+(?=s\s)', re.IGNORECASE)
+    re_usd = re.compile(r'\$[0-9]+\.[0-9]+')
+    re_desc = re.compile(r'<.*?>')
+    re_mah = re.compile(r'(?<=\s)[0-9]+(?=mah\s)', re.IGNORECASE)
+    re_cells = re.compile(r'(?<=\s)[0-9]+(?=s\s)', re.IGNORECASE)
 
     def __init__(self, **kwargs):
-        self.URL = kwargs['url']
-        self.LISTINGS = []
-        self.PAGES = []
+        self.url = kwargs['url']
+        self.listings = []
+        self.pages = []
+        self.index = None
 
     def get_index(self):
-        r = requests.get(self.URL)
-        if r.status_code == 200:
-            self.INDEX = BeautifulSoup(r.text)
+        """
+        Get the root index page
+        """
+        req = requests.get(self.url)
+        if req.status_code == 200:
+            self.index = BeautifulSoup(req.text)
         else:
             return None
 
     def get_pages(self):
-        tags = self.INDEX.findAll('form', {'class': 'paging'})
+        """
+        Compile a list of pages
+        """
+        tags = self.index.findAll('form', {'class': 'paging'})
         for tag in tags:
             lis = tag.findAll('li')
             for li in lis:
                 links = li.findAll('a')
                 for link in links:
-                    self.PAGES.append((link.text, link['href']))
+                    self.pages.append((link.text, link['href']))
 
-    def get_page(self, **kwargs):
-        return requests.get(kwargs['url'])
+    @staticmethod
+    def get_page(url):
+        """
+        Get a page.
+        ulr: The url to a page
+        """
+        return requests.get(url)
 
+    def parse_listing(self, listing_text):
+        """
+        Tear apart and store a listing
 
-    @classmethod
-    def parse_listing(self, listing):
+        :param listing_text: The textual contents of a listing
+        """
 
-        LISTING = {
+        listing = {
             'price': None,
             'cells': None,
             'capacity': None,
@@ -51,56 +66,69 @@ class Crawler:
         }
 
         # Description is in ANCHOR tag (has capacity, and cell count)
-        descriptiontags = listing.find_all('a')
-        for description in descriptiontags:
+        description_tags = listing_text.find_all('a')
+        for description in description_tags:
             text = description.renderContents().strip()
             if not self.re_desc.match(text):
                 try:
-                    LISTING['capacity'] = self.re_mah.findall(text)[0]
-                    LISTING['cells'] = self.re_cells.findall(text)[0]
-                    LISTING['link'] = 'http://www.hobbyking.com/hobbyking/store/' + description['href']
+                    listing['capacity'] = self.re_mah.findall(text)[0]
+                    listing['cells'] = self.re_cells.findall(text)[0]
+                    listing['link'] = (
+                        'http://www.hobbyking.com'
+                        '/hobbyking/store/'
+                    )
+                    listing['link'] += description['href']
+
                 except IndexError:
                     return None
 
         # Price is currently in FONT tag
-        fonttags = listing.findAll('font')
-        for fonttag in fonttags:
-            text = fonttag.renderContents()
+        font_tags = listing_text.findAll('font')
+        for font_tag in font_tags:
+            text = font_tag.renderContents()
             if self.re_usd.match(text):
-                LISTING['price'] = self.re_usd.findall(text)[0]
+                listing['price'] = self.re_usd.findall(text)[0]
 
-        return LISTING
+        return listing
 
+    def get_listings(self):
+        """
+        Compile all listings
+        """
 
-    def get_listings(self, **kwargs):
+        self.listings.append(self.parse_listing(self.index))
 
-        self.LISTINGS.append(self.parse_listing(self.INDEX))
+        for _page in self.pages:
 
-        for page in self.PAGES:
-
-            listings = page[2].findAll('tr', {'class': 'zeroLineHeight'})
+            listings = _page[2].findAll('tr', {'class': 'zeroLineHeight'})
 
             for listing in listings:
                 if listing:
-                    self.LISTINGS.append(self.parse_listing(listing))
+                    self.listings.append(self.parse_listing(listing))
 
 
 if __name__ == '__main__':
     import settings
     import json
 
-    crawler = Crawler(url=settings.URL)
-    crawler.get_index()
+    CRAWLER = Crawler(url=settings.URL)
+    CRAWLER.get_index()
 
     # For "production"
-    crawler.get_pages()
-    for i, page in enumerate(crawler.PAGES):
-        crawler.PAGES[i] = (page[0], page[1], BeautifulSoup(crawler.get_page(url='http://www.hobbyking.com/hobbyking/store/'+page[1]).text))
+    CRAWLER.get_pages()
+    for i, page in enumerate(CRAWLER.pages):
+        CRAWLER.pages[i] = (
+            page[0],
+            page[1],
+            BeautifulSoup(CRAWLER.get_page(
+                url='http://www.hobbyking.com/hobbyking/store/'+page[1]
+            ).text
+            )
+        )
 
     # For testing
-    #crawler.PAGES.append((1, settings.URL, crawler.INDEX))
+    #CRAWLER.PAGES.append((1, settings.URL, CRAWLER.INDEX))
 
-    crawler.get_listings()
+    CRAWLER.get_listings()
 
-    print json.dumps(crawler.LISTINGS)
-
+    print json.dumps(CRAWLER.listings)
